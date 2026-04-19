af5bfbd41ef60a1138aa8c4678423eccd2ef725d
Fezile Gulwa <fezilegulwa@gmail.com>
2026-04-19 15:34:04 +0200

    Callbacks and Error Handling

--- dist/rectangle.dev.js
+++ dist/rectangle.dev.js
@@ -1,10 +1,17 @@
 "use strict";
 
-exports.perimeter = function (x, y) {
-  return 2 * (x + y);
-};
-
-exports.area = function (x, y) {
-  return x * y;
+module.exports = function (x, y, callback) {
+  if (x <= 0 || y <= 0) setTimeout(function () {
+    return callback(new Error("Rectangle dimensions should be greater than zero: l = " + x + ", and b = " + y), null);
+  }, 2000);else setTimeout(function () {
+    return callback(null, {
+      perimeter: function perimeter() {
+        return 2 * (x + y);
+      },
+      area: function area() {
+        return x * y;
+      }
+    });
+  }, 2000);
 };
 //# sourceMappingURL=rectangle.dev.js.map
